69970c2882d753ef10209bed0b526f53167386a1
sharpar <shivain27@gmail.com>
2020-12-05 19:09:51 -0500

    Start building the main app, add rough structure and styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
 
+const CITIES = {
+	dubai: { name: 'Dubai', country: 'UAE', lat: 25.055576, long: 55.144069 },
+	toronto: { name: 'Toronto', country: 'Canada', lat: 43.6594, long: -79.3851 },
+	sandys: { name: 'Sandys', country: 'Bermuda', lat: 32.2797, long: -64.874 },
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from the weather app!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [currentCity, setCurrentCity] = useState('toronto');
+	const cityIds = Object.keys(CITIES);
+
+	return (
+		<div className="App">
+
+			<main className='main'>
+
+				<div className={'city-options'}>
+					{cityIds.map((cityId) => {
+						const cityInfo = CITIES[cityId];
+						const isSelected = cityId === currentCity;
+
+						return (
+							<div
+								key={cityInfo.name + cityInfo.country}
+								className={`city ${isSelected ? 'selected' : ''}`}
+								onClick={() => setCurrentCity(cityId)}
+							>
+								<div className="city-name">{cityInfo.name}</div>
+								<div className="country-name">{cityInfo.country}</div>
+							</div>
+						);
+					})}
+				</div>
+
+				<div className="container--weather">
+					<div className="current-weather">Today</div>
+					<div className="container--week-forecast">
+						<div className="day-forecast">Wed 17</div>
+						<div className="day-forecast">Thu 12</div>
+						<div className="day-forecast">Fri -1</div>
+						<div className="day-forecast">Sat 23</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;

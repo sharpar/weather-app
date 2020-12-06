4b4ce78ad242fbfbfa57ecc6d49abab38aefbc33
sharpar <shivain27@gmail.com>
2020-12-06 12:48:30 -0500

    Fetch data from Dark Sky, add a sample data json file for reference

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import githubIcon from './github-mark.svg';
+import sampleData from './sampleData.json';
 import './App.scss';
+
+// Ideally, this key would not be saved here. It is not secure to expose it on
+// the front end and in the code because this will be a public repository.
+// It should be saved in a backend API, that the React app would make a request
+// to, the API would then fetch the data from Dark Sky.
+// Fortunately, no payment method is attached to the account so the key will
+// stop working after a 1000 calls.
+const DARK_SKY_KEY = 'de58b48418b7a1930004d32486ff7c93';
 
 const CITIES = {
 	dubai: { name: 'Dubai', country: 'UAE', lat: 25.055576, long: 55.144069 },
@@ -10,7 +19,36 @@ const CITIES = {
 
 function App() {
 	const [currentCity, setCurrentCity] = useState('toronto');
-	const cityIds = Object.keys(CITIES);
+	const [weatherData, setWeatherData] = useState(null);
+
+	useEffect(() => {
+		console.log(`Lets fetch data for ${currentCity}!`);
+		const { lat, long } = CITIES[currentCity];
+
+		async function fetchData() {
+			// CORS issues with dark sky (another symptom of making the calls
+			// directly from the client):
+			// https://forum.freecodecamp.org/t/solved-having-trouble-getting-response-from-dark-sky-api/100653/5
+			const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${DARK_SKY_KEY}/${lat},${long}?units=ca`;
+
+			const result = await fetch(url);
+
+			const data = await result.json();
+			console.log({ result, data });
+			return data;
+		}
+
+		fetchData()
+			.then((data) => setWeatherData(data))
+			.catch((err) => {
+				console.log(err);
+				// alert(
+				// 	'Fetching weather failed, using dummy data. ' +
+				// 		'Please refresh page to try again.',
+				// );
+				setWeatherData(sampleData);
+			});
+	}, [currentCity]);
 
 	return (
 		<div className="App">
